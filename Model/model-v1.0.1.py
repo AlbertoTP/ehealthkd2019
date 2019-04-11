4f6fbbe003d1c9745b2a3435d18e4c96294a0e3b
AlbertoTP <alberto.tapia.p@gmail.com>
2019-04-11 14:00:35 -0500

    model v1.0.1

--- Model/model-v1.0.1.py
+++ Model/model-v1.0.1.py
@@ -92,8 +92,10 @@ DicReference={
 #5Head POS
 #6Children
 #7type
+#8Word Start
+#9Word End
 """
-def isConcept(element,cid):
+def isConcept(element):
     #Concept
     if (element[2] in DicConcept):
         #Dep with child
@@ -101,46 +103,45 @@ def isConcept(element,cid):
             for parent in element[6]:
                 #print (">>>",parent,parent.pos_,parent.dep_)
                 if ( (element[3] in DicConcept) or (parent.dep_ in DicConcept) ):
-                    print (str(cid)+"\t"+"Concept\t"+"#,#"+"\t"+element[1])
+                    #print ("\t"+"Concept\t"+"#,#"+"\t"+element[1])
                     return "Concept"
         #Dep with parent
         else:
             if ( (element[3] in DicConcept) or (element[5]=="ADJ") ):
-                print (str(cid)+"\t"+"Concept\t"+"#,#"+"\t"+element[1])
+                #print ("\t"+"Concept\t"+"#,#"+"\t"+element[1])
                 return "Concept"
     else:
         return False
     
-def isAction(element,cid):
+def isAction(element):
     #Action
     if ( ((element[2] in DicAction) and (element[3] in DicAction)) or element[2] =="VERB" ):
-        print (str(cid)+"\t"+"Action\t"+"#,#"+"\t"+element[1])
+        #print ("\t"+"Action\t"+"#,#"+"\t"+element[1])
         return "Action"
     else:
         return False
 
-def isReference(element,cid):
+def isReference(element):
     #Reference
     if ( (element[2] in DicReference) and (element[3] in DicReference) ):
-        print (str(cid)+"\t"+"Reference\t"+"#,#"+"\t"+element[1])
+        #print ("\t"+"Reference\t"+"#,#"+"\t"+element[1])
         return "Reference"
     else:
         return False
 
-def taggging(printline,cid):
+def taggging(printline):
     for element in printline:
         #print (element)
         tmp=False
         #Concept
-        tmp=isConcept(element,cid)
+        tmp=isConcept(element)
         if (tmp):
             element[7]=tmp
-            cid+=1
-        #Action
-        tmp=isAction(element,cid)
-        if (tmp):
-            element[7]=tmp
-            cid+=1
+        else:
+            #Action
+            tmp=isAction(element)
+            if (tmp):
+                element[7]=tmp
         #Predicate
         if ((element[2] in DicPredicate) and (element[3] in DicPredicate)):
             #tener referencia de un concepto
@@ -151,26 +152,23 @@ def taggging(printline,cid):
                     if (element[4]==i[1]):                            
                         #print (">>>>>>>>>>",i)
                         if (i[7]=="Concept"):
-                            print (str(cid)+"\t"+"Predicate\t"+"#,#"+"\t",element[1])
+                            #print ("\t"+"Predicate\t"+"#,#"+"\t",element[1])
                             element[7]="Predicate"
-                            cid+=1
                         else:
-                            if (isConcept(i,cid)=="Concept"):
-                                print (str(cid)+"\t"+"Predicate\t"+"#,#"+"\t",element[1])
+                            if (isConcept(i)=="Concept"):
+                                #print ("\t"+"Predicate\t"+"#,#"+"\t"+element[1])
                                 element[7]="Predicate"
-                                cid+=1
-        #Action
-        tmp=isReference(element,cid)
+        #Reference
+        tmp=isReference(element)
         if (tmp):
             element[7]=tmp
-            cid+=1
-    return cid
+    return printline
 
 def main():
     starting_point = time.time()
     print ("eHealth-KD Challenge 2019")    
-    #path="trial/input_trial.txt"
-    path="trial/example.txt"
+    path="trial/input_trial.txt"
+    #path="trial/example.txt"
     #res="result-v1.0.1.txt"
     try:
         file = open(path,"r")
@@ -181,6 +179,7 @@ def main():
 
     ind=1
     cid=1
+    cont_tot=0
     #print ("ID\tSTART/END\tLABEL\tTEXT (optional)")
     for linea in file.readlines():
         line=linea.rstrip('\n')#.split(" ")
@@ -194,6 +193,7 @@ def main():
         LabelLine=[]
         for token in doc:
             #print(token.text,"\t",token.pos_,"\t",token.dep_,"\t",token.head.text,"\t",token.head.pos_,"\t",[child for child in token.children])
+            
             #delete the tag of the token
             if not((token.pos_ in DicDelTags) or (token.dep_ in DicDelTags)):
                 
@@ -204,11 +204,21 @@ def main():
                     if not((ch.pos_ in DicDelTags) or (ch.dep_ in DicDelTags)):
                         child.append(ch)
                 #print (child)
-                LabelLine.append([indWord,token.text,token.pos_,token.dep_,token.head.text,token.head.pos_,child,''])
+                word_start=cont_tot+line.index(token.text)
+                word_end=word_start+len(token.text)
+                #print (word_start,word_end)
+                LabelLine.append([indWord,token.text,token.pos_,token.dep_,token.head.text,token.head.pos_,child,'',word_start,word_end])
 
             indWord+=1
         ind+=1
-        cid=taggging(LabelLine,cid)
+        tags=taggging(LabelLine)
+        for element in tags:
+            if (element[7]!=''):
+                #print (str(cid),element)
+                print (str(cid)+"\t"+str(element[7])+"\t"+str(element[8])+","+str(element[9])+"\t"+str(element[1]))
+                cid+=1
+        
+        cont_tot+=len(line)
 
 
     file.close()
